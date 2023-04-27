3b19b9033de9a155d371ece7d569f9a71a08ed81
Murat Akdağ <75887798+muratakdag@users.noreply.github.com>
2023-04-27 16:51:28 +0300

    Parse Form Bodies

--- server/build/index.js
+++ server/build/index.js
@@ -3,10 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var loginRoutes_1 = require("./routes/loginRoutes");
-var app = (0, express_1.default)();
+const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
+const body_parser_1 = __importDefault(require("body-parser"));
+const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(loginRoutes_1.router);
-app.listen(3000, function () {
+app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
